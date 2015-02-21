794940e2aa4739ff0fcfe5fc7262e9a5c4b0f611
carlislerainey <carlislerainey@gmail.com>
2015-02-21 18:59:49 -0500

    add lts illustration to text

--- R/lts-illustration.R
+++ R/lts-illustration.R
@@ -1,4 +1,7 @@
 
+# set working directory
+setwd("~/Dropbox/projects/heavy-tails")
+
 # clear workspace
 rm(list = ls())
 
@@ -8,8 +11,8 @@ library(galts)
 
 # parameters
 n <- 50
-n.sims <- 1000
-df.t <- 2.5
+n.sims <- 10000
+df.t <- 3
 
 # perform simulation
 set.seed(89132)
@@ -36,32 +39,34 @@ sd(b.hat.ls); sd(b.hat.lts)
 
 
 # plot
-par(mfrow = c(1, 2), oma = c(3, 3, 1, 1), mar = c(1, 1, 1, 1))
-h.ls <- hist(b.hat.ls)
-h.lts <- hist(b.hat.lts)
+pdf("doc/figs/lts-illustration.pdf", height = 3.5, width = 8)
+par(mfrow = c(1, 2), oma = c(2, 2, 1/2, 1/2), mar = c(1, 1, 1, 1))
+h.ls <- hist(b.hat.ls, plot = FALSE)
+h.lts <- hist(b.hat.lts, plot = FALSE)
 breaks.at <- seq(from = min(c(h.ls$breaks, h.lts$breaks)),
                  to = max(c(h.ls$breaks, h.lts$breaks)),
-                 length.out = 50)
-h.ls <- hist(b.hat.ls, breaks = breaks.at)
-h.lts <- hist(b.hat.lts, breaks = breaks.at)
+                 length.out = 100)
+h.ls <- hist(b.hat.ls, breaks = breaks.at, plot = FALSE)
+h.lts <- hist(b.hat.lts, breaks = breaks.at, plot = FALSE)
 
 eplot(xlim = mm(c(h.ls$breaks, h.lts$breaks)),
-      ylim = mm(c(h.ls$density, h.lts$density)),
+      ylim = 1.05*mm(c(h.ls$density, h.lts$density)),
+      ylabpos = 2.0,
       xlab = "Coefficient Estimates",
       ylab = "Counts",
       main = "Least Squares")
-plot(h.ls, freq = FALSE, add = TRUE, border = NA, col = "grey50")
-lines(density(b.hat.ls), lwd = 3)
+plot(h.ls, freq = FALSE, add = TRUE, border = "black", col = "black")
+#lines(density(b.hat.ls), lwd = 2)
 text(par("usr")[2], .8*par("usr")[4], paste("True = 1.00\nMean = ", format(round(mean(b.hat.ls), 2), nsmall = 2), 
                  "\nSD = ", format(round(sd(b.hat.ls), 2), nsmall = 2),
                  "\nMSE = ", format(round(sum((b.hat.ls - b)^2), 2), nsmall = 2), sep = ""), 
      pos = 2, cex = 0.7)
 aplot("Least Trimmed Squares")
-plot(h.lts, freq = FALSE, add = TRUE, border = NA, col = "grey50")
-lines(density(b.hat.lts), lwd = 3)
+plot(h.lts, freq = FALSE, add = TRUE, border = "black", col = "black")
+#lines(density(b.hat.lts), lwd = 2)
 text(par("usr")[2], .8*par("usr")[4], paste("True = 1.00\nMean = ", format(round(mean(b.hat.lts), 2), nsmall = 2), 
                              "\nSD = ", format(round(sd(b.hat.lts), 2), nsmall = 2),
                              "\nMSE = ", format(round(sum((b.hat.lts - b)^2), 2), nsmall = 2), sep = ""), 
      pos = 2, cex = 0.7)
-
+dev.off()
 
